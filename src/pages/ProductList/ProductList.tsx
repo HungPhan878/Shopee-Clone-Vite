ea972b4cdd4ff3138fbbc9183088ef0e1802c2a7
Phan Van Hung <Hungphanhung8@gmail.com>
2024-05-15 22:52:03 +0700

    create compoent Carousel.tsx and make UI slideshow for productList page

--- src/pages/ProductList/ProductList.tsx
+++ src/pages/ProductList/ProductList.tsx
@@ -2,15 +2,37 @@ import classNames from 'classnames/bind'
 
 // scss
 import style from './ProductList.module.scss'
+
+// components
 import AsideFilter from './Components/AsideFilter'
 import SortProductList from './Components/SortProductList'
 import Product from './Components/Product'
+import Carousel from 'src/Components/Carousel'
 
 const cx = classNames.bind(style)
+
+const slides = [
+  'https://cf.shopee.vn/file/vn-50009109-7a39522ed87cf07fc399dc600fb0587d_xxhdpi',
+  'https://cf.shopee.vn/file/vn-50009109-fb6981047fd0fb38c526ec567aa6071b_xxhdpi',
+  'https://cf.shopee.vn/file/vn-50009109-3ea9262b135bdb95adf248cfcf63b204_xxhdpi',
+  'https://cf.shopee.vn/file/vn-50009109-0fc7dda14ad435b3daff36b6c864bc8d_xxhdpi'
+]
 
 export default function ProductList() {
   return (
     <div>
+      <section className={cx('product-slides__wrap')}>
+        <div className='container'>
+          <div className={cx('product-slides__inner')}>
+            <Carousel autoSlides={true}>
+              {slides.map((s, index) => (
+                <img className={cx('product-slides__img')} src={s} key={index} alt='slides' />
+              ))}
+            </Carousel>
+          </div>
+        </div>
+      </section>
+
       <div className={cx('product-list__wrap')}>
         <div className='container'>
           <div className='row'>
