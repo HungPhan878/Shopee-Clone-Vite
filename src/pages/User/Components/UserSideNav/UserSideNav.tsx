61e92cafa9bc20ba36857d6e306b7bfd4e9bf841
Phan Van Hung <Hungphanhung8@gmail.com>
2024-06-07 22:36:25 +0700

    Code UI UserSideNav

--- src/pages/User/Components/UserSideNav/UserSideNav.tsx
+++ src/pages/User/Components/UserSideNav/UserSideNav.tsx
@@ -1,3 +1,104 @@
+import classNames from 'classnames/bind'
+
+// scss
+import style from './UserSideNav.module.scss'
+import { Link } from 'react-router-dom'
+
+// components
+
+const cx = classNames.bind(style)
+
 export default function UserSideNav() {
-  return <div>UserSideNav</div>
+  return (
+    <div>
+      <div className={cx('nav__row')}>
+        <div className={cx('nav-img__wrap')}>
+          <img
+            src='https://api-ecom.duthanhduoc.com/images/6a38bb64-32c2-492a-b729-bf0529bd7438.jpg'
+            alt='avatar'
+            className={cx('nav-img__avatar')}
+          />
+        </div>
+        <div className={cx('nav-info')}>
+          <div className={cx('nav-info__name')}>Hungphanhung8@gmail.com</div>
+          <Link to='#' className={cx('nav-info__link')}>
+            <svg
+              width={12}
+              height={12}
+              viewBox='0 0 12 12'
+              xmlns='http://www.w3.org/2000/svg'
+              style={{ marginRight: 4 }}
+            >
+              <path
+                d='M8.54 0L6.987 1.56l3.46 3.48L12 3.48M0 8.52l.073 3.428L3.46 12l6.21-6.18-3.46-3.48'
+                fill='#9B9B9B'
+                fillRule='evenodd'
+              />
+            </svg>
+            Sửa hồ sơ
+          </Link>
+        </div>
+      </div>
+
+      <ul className={cx('nav__list')}>
+        <li className={cx('nav-item')}>
+          <Link to='#' className={cx('nav-item__link')}>
+            <svg
+              xmlns='http://www.w3.org/2000/svg'
+              fill='none'
+              viewBox='0 0 24 24'
+              strokeWidth={1.5}
+              stroke='currentColor'
+              className={cx('nav-item__icon')}
+            >
+              <path
+                strokeLinecap='round'
+                strokeLinejoin='round'
+                d='M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z'
+              />
+            </svg>
+            Tài khoản của tôi
+          </Link>
+        </li>
+        <li className={cx('nav-item')}>
+          <Link to='#' className={cx('nav-item__link')}>
+            <svg
+              xmlns='http://www.w3.org/2000/svg'
+              fill='none'
+              viewBox='0 0 24 24'
+              strokeWidth={1.5}
+              stroke='currentColor'
+              className={cx('nav-item__icon')}
+            >
+              <path
+                strokeLinecap='round'
+                strokeLinejoin='round'
+                d='M16.5 10.5V6.75a4.5 4.5 0 1 0-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 0 0 2.25-2.25v-6.75a2.25 2.25 0 0 0-2.25-2.25H6.75a2.25 2.25 0 0 0-2.25 2.25v6.75a2.25 2.25 0 0 0 2.25 2.25Z'
+              />
+            </svg>
+            Đổi mật khẩu
+          </Link>
+        </li>
+        <li className={cx('nav-item')}>
+          <Link to='#' className={cx('nav-item__link')}>
+            <svg
+              xmlns='http://www.w3.org/2000/svg'
+              fill='none'
+              viewBox='0 0 24 24'
+              strokeWidth={1.5}
+              stroke='currentColor'
+              className={cx('nav-item__icon')}
+            >
+              <path
+                strokeLinecap='round'
+                strokeLinejoin='round'
+                d='M9 12h3.75M9 15h3.75M9 18h3.75m3 .75H18a2.25 2.25 0 0 0 2.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 0 0-1.123-.08m-5.801 0c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 0 0 .75-.75 2.25 2.25 0 0 0-.1-.664m-5.8 0A2.251 2.251 0 0 1 13.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m0 0H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V9.375c0-.621-.504-1.125-1.125-1.125H8.25ZM6.75 12h.008v.008H6.75V12Zm0 3h.008v.008H6.75V15Zm0 3h.008v.008H6.75V18Z'
+              />
+            </svg>
+            Đơn mua
+          </Link>
+        </li>
+      </ul>
+    </div>
+  )
 }
--- src/pages/User/layouts/UserLayout/UserLayout.tsx
+++ src/pages/User/layouts/UserLayout/UserLayout.tsx
@@ -1,11 +1,27 @@
 import { Outlet } from 'react-router-dom'
+import classNames from 'classnames/bind'
+
+// scss
+import style from './UserLayout.module.scss'
+
+// components
 import UserSideNav from '../../Components/UserSideNav'
+
+const cx = classNames.bind(style)
 
 export default function UserLayout() {
   return (
-    <div>
-      <UserSideNav />
-      <Outlet />
-    </div>
+    <main className={cx('wrapper')}>
+      <div className='container'>
+        <div className='row'>
+          <div className='col col-2 col-lg-3 col-md-12'>
+            <UserSideNav />
+          </div>
+          <div className='col col-10 col-lg-9 col-md-12'>
+            <Outlet />
+          </div>
+        </div>
+      </div>
+    </main>
   )
 }
