13da424ba532dd09fc7add3087ccbdc44865dfc3
Phan Van Hung <Hungphanhung8@gmail.com>
2024-06-21 22:47:58 +0700

    Test functions in util.ts file

--- src/utils/__test__/util.test.ts
+++ src/utils/__test__/util.test.ts
@@ -1,5 +1,8 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { AxiosError, isAxiosError } from 'axios'
+import HttpStatusCode from 'src/constants/HttpStatusCode.enum'
 import { expect, describe, it } from 'vitest'
+import { isAxiosUnprocessableEntityError } from '../util'
 
 //decribe là mô tả các ngữ cánh hoặc tên của các component, funtion cần test
 describe('isAxiosError', () => {
@@ -8,5 +11,29 @@ describe('isAxiosError', () => {
     // expect : là dùng để kì vọng kết quả trả về
     // Khi thay đổi logic trong code thì phải thay đôi file test luôn nha
     expect(isAxiosError(new AxiosError())).toBe(true)
+    expect(isAxiosError(new Error())).toBe(false)
+  })
+})
+
+describe('isAxiosUnprocessableEntityError', () => {
+  it('isAxiosUnprocessableEntityError return boolean', () => {
+    expect(
+      isAxiosUnprocessableEntityError(
+        new AxiosError(undefined, undefined, undefined, undefined, {
+          status: HttpStatusCode.UnprocessableEntity,
+          data: null
+        } as any)
+      )
+    ).toBe(true)
+
+    expect(
+      isAxiosUnprocessableEntityError(
+        new AxiosError(undefined, undefined, undefined, undefined, {
+          // đầy là cách truyền responsize có mã lối (status) trả về 500
+          status: HttpStatusCode.InternalServerError,
+          data: null
+        } as any)
+      )
+    ).toBe(false)
   })
 })
