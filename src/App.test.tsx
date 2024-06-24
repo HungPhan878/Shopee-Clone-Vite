757ff0be2fe442ecdafa803ac2ab5a7055bed1d1
Phan Van Hung <Hungphanhung8@gmail.com>
2024-06-24 21:41:47 +0700

    Test App render and React Router p2

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,4 +1,5 @@
-import { render, screen } from '@testing-library/react'
+/* eslint-disable prettier/prettier */
+import { render, screen, waitFor } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import * as matchers from '@testing-library/jest-dom/matchers'
 import { expect, describe, test } from 'vitest'
@@ -9,12 +10,27 @@ import { BrowserRouter } from 'react-router-dom'
 
 expect.extend(matchers)
 
-describe('App', async () => {
-  test('render app và chuyển trang', () => {
+describe('App', () => {
+  test('render app và chuyển trang', async () => {
+    const user = userEvent.setup()
+
     render(<App />, {
       wrapper: BrowserRouter
     })
 
-    screen.debug()
+    // Verify Trang chủ
+    await waitFor(() => {
+      expect(document.querySelector('title')?.textContent).toBe('Shopee Clone | Trang chủ')
+      expect(screen.getByText('sắp xếp theo')).toBeInTheDocument()
+    })
+
+    // Verify trang login
+    await user.click(screen.getByText(/đăng nhập/i))
+
+    await waitFor(() => {
+      expect(document.querySelector('title')?.textContent).toBe('Shopee Clone | Đăng Nhập')
+    })
+
+    screen.debug(document.body.parentElement as HTMLElement, 888888)
   })
 })
